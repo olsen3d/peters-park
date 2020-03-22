b6f43793690b13b708441e0a4f11041bf9ade7fc
Michael Olsen <molsen@Michaels-MacBook-Air.local>
2020-03-22 17:14:44 -0400

    redux cats

--- src/client/store/cat.js
+++ src/client/store/cat.js
@@ -3,19 +3,22 @@ import axios from "axios";
 // ACTION TYPES
 // YOU MAKE THEM!!
 
+const LOAD_CAT = 'LOAD_CAT'
 
 // INITIAL STATE
-const initialState = null
+const initialState = {}
 
 // ACTION CREATORS
 export const loadCat = data => ({
   // what kinda stuff goes in here?
+  type: LOAD_CAT, cat: data
 })
 
 // THUNK CREATORS
 export const fetchCat = id => async (dispatch) => {
   // YOUR CODE HERE
-
+  const cat = (await axios.get(`/api/cats/${id}/`)).data
+  return dispatch(loadCat(cat))
 }
 
 // REDUCER
@@ -23,7 +26,7 @@ export const fetchCat = id => async (dispatch) => {
 // don't modify what the function takes
 export default function (state = initialState, action) {
   switch (action.type) {
-
+    case LOAD_CAT: return action.cat
 
     default: return state;
   }
