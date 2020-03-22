c10df7269a3bd8b0f79b9586e7501606458732b3
Michael Olsen <molsen@Michaels-MacBook-Air.local>
2020-03-22 17:22:46 -0400

    donzo

--- src/client/store/cats.js
+++ src/client/store/cats.js
@@ -1,23 +1,30 @@
 import axios from 'axios'
+import { loadCat } from './cat'
+import { bindActionCreators } from 'redux'
 
 
 // ACTION TYPES
 // YOU MAKE THEM!!
 
+const LOAD_CATS = 'LOAD_CATS'
+
 // INITIAL STATE
 // what would be a good initial state?
 // :thinking:
-const initialState = null
+const initialState = []
 
 // ACTION CREATORS
 export const loadCats = data => ({
   // some stuff might go in here? hmmmmm.....
+  type: LOAD_CATS, cats: data
 })
 
 
 // THUNK CREATORS
 export const fetchCats = () => async dispatch => {
   // YOUR CODE HERE
+  const cats = (await axios.get('/api/cats/')).data
+  dispatch(loadCats(cats))
 }
 
 // REDUCER
@@ -25,7 +32,7 @@ export const fetchCats = () => async dispatch => {
 // don't modify what the function takes
 export default function (state = initialState, action) {
   switch (action.type) {
-
+    case LOAD_CATS: return action.cats
 
     default: return state;
   }
